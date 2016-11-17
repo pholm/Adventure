cc2f78ba2d9101001de2119e40d3fce1deb960b5
Petrus Holm <holm.petrus@gmail.com>
2016-11-17 11:24:44 +0200

    Poistettu bugi, jossa ryhmä ei liiku hahmon mukana alueiden välillä

--- o1/appropeli/Person.scala
+++ o1/appropeli/Person.scala
@@ -1,6 +1,6 @@
 package o1.appropeli
 
-class Person(val name: String, val sijainti: Area, val tunnuslause: String) {
+class Person(val name: String, var sijainti: Area, val tunnuslause: String) {
   
   override def toString = this.name
   
--- o1/appropeli/Player.scala
+++ o1/appropeli/Player.scala
@@ -105,7 +105,7 @@ class Player(startingArea: Area) {
     if (destination.isDefined) {
       this.kusi += 1 
       nykysijainti.onkoKayty = false
-      this.ryhmä.foreach(_.jasenet.forall(_.sijainti == destination))
+      this.ryhmä.foreach(_.jasenet.foreach(_.sijainti = destination.get))
       "Menit " + direction + "." 
     } else "Et voi mennä " + direction + "."
     } else "Yritit livahtaa kaveriesi ohi, mutta viime hetkellä aina kärppänä oleva Antti 'rontti' Ihalainen bongaa sinut. Joudut siis valitsemaan jonkun ryhmistä."
